642cb4e5b09104a0cfa249260b85249eccb8f7fc
AmrTarekElaasy <amrtarekelaasy@outlook.com>
2025-07-02 23:02:41 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace Car_Game
+{
+    partial class FrmCarGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmCarGame));
+            this.panelGame = new System.Windows.Forms.Panel();
+            this.labelGameOver = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.minCar = new System.Windows.Forms.PictureBox();
+            this.line2_2 = new System.Windows.Forms.PictureBox();
+            this.line2_1 = new System.Windows.Forms.PictureBox();
+            this.line2_0 = new System.Windows.Forms.PictureBox();
+            this.line1_2 = new System.Windows.Forms.PictureBox();
+            this.line0_2 = new System.Windows.Forms.PictureBox();
+            this.line1_1 = new System.Windows.Forms.PictureBox();
+            this.line0_1 = new System.Windows.Forms.PictureBox();
+            this.line1_0 = new System.Windows.Forms.PictureBox();
+            this.line0_0 = new System.Windows.Forms.PictureBox();
+            this.timerSpeed = new System.Windows.Forms.Timer(this.components);
+            this.timerBotCar = new System.Windows.Forms.Timer(this.components);
+            this.timerScore = new System.Windows.Forms.Timer(this.components);
+            this.labelScore = new System.Windows.Forms.Label();
+            this.labelMaxScore = new System.Windows.Forms.Label();
+            this.labelSpeed = new System.Windows.Forms.Label();
+            this.panelGame.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minCar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.line2_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.line2_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.line2_0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.line1_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.line0_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.line1_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.line0_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.line1_0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.line0_0)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelGame
+            // 
+            this.panelGame.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panelGame.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelGame.Controls.Add(this.minCar);
+            this.panelGame.Controls.Add(this.labelGameOver);
+            this.panelGame.Controls.Add(this.pictureBox3);
+            this.panelGame.Controls.Add(this.pictureBox2);
+            this.panelGame.Controls.Add(this.pictureBox1);
+            this.panelGame.Controls.Add(this.line2_2);
+            this.panelGame.Controls.Add(this.line2_1);
+            this.panelGame.Controls.Add(this.line2_0);
+            this.panelGame.Controls.Add(this.line1_2);
+            this.panelGame.Controls.Add(this.line0_2);
+            this.panelGame.Controls.Add(this.line1_1);
+            this.panelGame.Controls.Add(this.line0_1);
+            this.panelGame.Controls.Add(this.line1_0);
+            this.panelGame.Controls.Add(this.line0_0);
+            this.panelGame.Location = new System.Drawing.Point(0, 38);
+            this.panelGame.Name = "panelGame";
+            this.panelGame.Size = new System.Drawing.Size(871, 731);
+            this.panelGame.TabIndex = 0;
+            this.panelGame.Paint += new System.Windows.Forms.PaintEventHandler(this.panelGame_Paint);
+            // 
+            // labelGameOver
+            // 
+            this.labelGameOver.AutoSize = true;
+            this.labelGameOver.BackColor = System.Drawing.Color.White;
+            this.labelGameOver.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelGameOver.Location = new System.Drawing.Point(334, 237);
+            this.labelGameOver.Name = "labelGameOver";
+            this.labelGameOver.Size = new System.Drawing.Size(162, 31);
+            this.labelGameOver.TabIndex = 13;
+            this.labelGameOver.Text = "Game Over";
+            this.labelGameOver.Visible = false;
+            this.labelGameOver.Click += new System.EventHandler(this.labelGameOver_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(110, 180);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(57, 114);
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(89, 463);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(57, 114);
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(711, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(57, 114);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // minCar
+            // 
+            this.minCar.BackColor = System.Drawing.Color.Transparent;
+            this.minCar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("minCar.BackgroundImage")));
+            this.minCar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.minCar.Location = new System.Drawing.Point(401, 324);
+            this.minCar.Name = "minCar";
+            this.minCar.Size = new System.Drawing.Size(54, 114);
+            this.minCar.TabIndex = 6;
+            this.minCar.TabStop = false;
+            // 
+            // line2_2
+            // 
+            this.line2_2.BackColor = System.Drawing.Color.White;
+            this.line2_2.Location = new System.Drawing.Point(636, 550);
+            this.line2_2.Name = "line2_2";
+            this.line2_2.Size = new System.Drawing.Size(18, 162);
+            this.line2_2.TabIndex = 9;
+            this.line2_2.TabStop = false;
+            // 
+            // line2_1
+            // 
+            this.line2_1.BackColor = System.Drawing.Color.White;
+            this.line2_1.Location = new System.Drawing.Point(422, 550);
+            this.line2_1.Name = "line2_1";
+            this.line2_1.Size = new System.Drawing.Size(18, 162);
+            this.line2_1.TabIndex = 8;
+            this.line2_1.TabStop = false;
+            // 
+            // line2_0
+            // 
+            this.line2_0.BackColor = System.Drawing.Color.White;
+            this.line2_0.Location = new System.Drawing.Point(207, 550);
+            this.line2_0.Name = "line2_0";
+            this.line2_0.Size = new System.Drawing.Size(18, 162);
+            this.line2_0.TabIndex = 7;
+            this.line2_0.TabStop = false;
+            // 
+            // line1_2
+            // 
+            this.line1_2.BackColor = System.Drawing.Color.White;
+            this.line1_2.Location = new System.Drawing.Point(636, 296);
+            this.line1_2.Name = "line1_2";
+            this.line1_2.Size = new System.Drawing.Size(18, 162);
+            this.line1_2.TabIndex = 5;
+            this.line1_2.TabStop = false;
+            this.line1_2.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // line0_2
+            // 
+            this.line0_2.BackColor = System.Drawing.Color.White;
+            this.line0_2.Location = new System.Drawing.Point(636, 42);
+            this.line0_2.Name = "line0_2";
+            this.line0_2.Size = new System.Drawing.Size(18, 162);
+            this.line0_2.TabIndex = 4;
+            this.line0_2.TabStop = false;
+            // 
+            // line1_1
+            // 
+            this.line1_1.BackColor = System.Drawing.Color.White;
+            this.line1_1.Location = new System.Drawing.Point(422, 296);
+            this.line1_1.Name = "line1_1";
+            this.line1_1.Size = new System.Drawing.Size(18, 162);
+            this.line1_1.TabIndex = 3;
+            this.line1_1.TabStop = false;
+            this.line1_1.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // line0_1
+            // 
+            this.line0_1.BackColor = System.Drawing.Color.White;
+            this.line0_1.Location = new System.Drawing.Point(422, 42);
+            this.line0_1.Name = "line0_1";
+            this.line0_1.Size = new System.Drawing.Size(18, 162);
+            this.line0_1.TabIndex = 2;
+            this.line0_1.TabStop = false;
+            // 
+            // line1_0
+            // 
+            this.line1_0.BackColor = System.Drawing.Color.White;
+            this.line1_0.Location = new System.Drawing.Point(207, 296);
+            this.line1_0.Name = "line1_0";
+            this.line1_0.Size = new System.Drawing.Size(18, 162);
+            this.line1_0.TabIndex = 1;
+            this.line1_0.TabStop = false;
+            // 
+            // line0_0
+            // 
+            this.line0_0.BackColor = System.Drawing.Color.White;
+            this.line0_0.Location = new System.Drawing.Point(208, 42);
+            this.line0_0.Name = "line0_0";
+            this.line0_0.Size = new System.Drawing.Size(18, 162);
+            this.line0_0.TabIndex = 0;
+            this.line0_0.TabStop = false;
+            // 
+            // timerSpeed
+            // 
+            this.timerSpeed.Enabled = true;
+            this.timerSpeed.Interval = 5;
+            this.timerSpeed.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timerBotCar
+            // 
+            this.timerBotCar.Enabled = true;
+            this.timerBotCar.Interval = 10;
+            this.timerBotCar.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timerScore
+            // 
+            this.timerScore.Enabled = true;
+            this.timerScore.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // labelScore
+            // 
+            this.labelScore.AutoSize = true;
+            this.labelScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelScore.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelScore.Location = new System.Drawing.Point(728, 9);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(81, 20);
+            this.labelScore.TabIndex = 1;
+            this.labelScore.Text = "Score : 0";
+            // 
+            // labelMaxScore
+            // 
+            this.labelMaxScore.AutoSize = true;
+            this.labelMaxScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMaxScore.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelMaxScore.Location = new System.Drawing.Point(31, 9);
+            this.labelMaxScore.Name = "labelMaxScore";
+            this.labelMaxScore.Size = new System.Drawing.Size(118, 20);
+            this.labelMaxScore.TabIndex = 2;
+            this.labelMaxScore.Text = "Max Score : 0";
+            // 
+            // labelSpeed
+            // 
+            this.labelSpeed.AutoSize = true;
+            this.labelSpeed.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSpeed.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelSpeed.Location = new System.Drawing.Point(384, 9);
+            this.labelSpeed.Name = "labelSpeed";
+            this.labelSpeed.Size = new System.Drawing.Size(86, 20);
+            this.labelSpeed.TabIndex = 3;
+            this.labelSpeed.Text = "Speed : 0";
+            // 
+            // FrmCarGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(871, 501);
+            this.Controls.Add(this.labelSpeed);
+            this.Controls.Add(this.labelMaxScore);
+            this.Controls.Add(this.labelScore);
+            this.Controls.Add(this.panelGame);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "FrmCarGame";
+            this.Text = "Car_Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FrmCarGame_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.FrmCarGame_KeyUp);
+            this.panelGame.ResumeLayout(false);
+            this.panelGame.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minCar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.line2_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.line2_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.line2_0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.line1_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.line0_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.line1_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.line0_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.line1_0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.line0_0)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelGame;
+        private System.Windows.Forms.PictureBox line1_2;
+        private System.Windows.Forms.PictureBox line0_2;
+        private System.Windows.Forms.PictureBox line1_1;
+        private System.Windows.Forms.PictureBox line0_1;
+        private System.Windows.Forms.PictureBox line1_0;
+        private System.Windows.Forms.PictureBox line0_0;
+        private System.Windows.Forms.Timer timerSpeed;
+        private System.Windows.Forms.PictureBox minCar;
+        private System.Windows.Forms.PictureBox line2_2;
+        private System.Windows.Forms.PictureBox line2_1;
+        private System.Windows.Forms.PictureBox line2_0;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timerBotCar;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label labelGameOver;
+        private System.Windows.Forms.Timer timerScore;
+        private System.Windows.Forms.Label labelScore;
+        private System.Windows.Forms.Label labelMaxScore;
+        private System.Windows.Forms.Label labelSpeed;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace Car_Game
+{
+    public partial class FrmCarGame : Form
+    {
+        public FrmCarGame()
+        {
+            InitializeComponent();
+        }
+        enum enDirctionLAndR { eL, eR, eNull }
+        enum enDirctionUAndD { eU, eD, eNull }
+
+        enDirctionLAndR _DirctionLAndR = enDirctionLAndR.eNull;
+        enDirctionUAndD _DirctionUAndD = enDirctionUAndD.eNull;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+        int _MainSpeed = 5;
+        int Speed()
+        {
+            return _MainSpeed+(_Score/500);
+        }
+        bool IsThereAnIntersection()
+        {
+            if (minCar.Bounds.IntersectsWith(pictureBox1.Bounds))
+            {
+                return true;
+            }
+            else if (minCar.Bounds.IntersectsWith(pictureBox2.Bounds))
+            {
+                return true;
+            }
+            else if (minCar.Bounds.IntersectsWith(pictureBox3.Bounds))
+            {
+                return true;
+            }
+
+
+            return false;
+        }
+        string stringOfSpeed()
+        {
+            return $"Speed : {Speed()}";
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+
+            line0_0.Top += Speed();
+            line0_1.Top += Speed();
+            line0_2.Top += Speed();
+
+            line1_0.Top += Speed();
+            line1_1.Top += Speed();
+            line1_2.Top += Speed();
+
+            line2_0.Top += Speed();
+            line2_1.Top += Speed();
+            line2_2.Top += Speed();
+
+            labelSpeed.Text = stringOfSpeed();
+
+
+            if (line0_0.Top + line0_0.Height > panelGame.Height)
+            {
+                line0_0.Top = 0 - line1_0.Height;
+                line0_1.Top = 0 - line1_1.Height;
+                line0_2.Top = 0 - line1_2.Height;
+            }
+
+            if (line1_0.Top + line1_0.Height > panelGame.Height)
+            {
+                line1_0.Top = 0 - line1_0.Height;
+                line1_1.Top = 0 - line1_1.Height;
+                line1_2.Top = 0 - line1_2.Height;
+            }
+
+            if (line2_0.Top + line2_0.Height > panelGame.Height)
+            {
+                line2_0.Top = 0 - line1_0.Height;
+                line2_1.Top = 0 - line1_1.Height;
+                line2_2.Top = 0 - line1_2.Height;
+            }
+
+
+            if (_DirctionLAndR == enDirctionLAndR.eL && minCar.Left > 0)
+            {
+                minCar.Left -= Speed();
+            }
+            else if (_DirctionLAndR == enDirctionLAndR.eR && minCar.Left < (panelGame.Right - minCar.Width))
+            {
+                minCar.Left += Speed();
+
+            }
+            if (_DirctionUAndD == enDirctionUAndD.eU && minCar.Top > 0)
+            {
+                minCar.Top -= Speed();
+            }
+            else if (_DirctionUAndD == enDirctionUAndD.eD && (minCar.Top + minCar.Height) < (this.Height - minCar.Height))
+            {
+                minCar.Top += Speed();
+
+            }
+
+           if (IsThereAnIntersection())
+           {
+               labelGameOver.Visible = true;
+               timerSpeed.Stop();
+               timerBotCar.Stop();
+               timerScore.Stop();
+               
+              
+         
+           }
+
+
+        }
+
+        private void panelGame_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        byte _MaxSpeedMinCarUAndD = 15;
+
+        void speedUpBotCar()
+        {
+            if (speedBotCar > -20)
+                speedBotCar--;
+        }
+        private void FrmCarGame_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (labelGameOver.Visible)
+            {
+                if ((e.KeyData == Keys.Space || e.KeyData == Keys.Enter || e.KeyData == Keys.Up))
+                {
+                    restartTheGame();
+                }
+            }
+            
+           else if (e.KeyData == Keys.Left)
+            {
+                _DirctionLAndR = enDirctionLAndR.eL;
+            }
+            else if (e.KeyData == Keys.Right)
+            {
+                _DirctionLAndR = enDirctionLAndR.eR;
+
+
+            }
+            else
+                _DirctionLAndR = enDirctionLAndR.eNull;
+
+
+            //  if (e.KeyData == Keys.Up)
+            //  {
+            //      //_DirctionUAndD = enDirctionUAndD.eU;
+            //
+            //      if (_Speed < _MaxSpeedMinCarUAndD)
+            //          _Speed++;
+            //      speedUpBotCar();
+            //  }
+            //  else if (e.KeyData == Keys.Down)
+            //  {
+            //      // _DirctionUAndD = enDirctionUAndD.eD;
+            //      if (_Speed > 0)
+            //          _Speed--;
+            //
+            //      // speedBotCar++;
+            //  }
+            //  else
+            //      _DirctionUAndD = enDirctionUAndD.eNull;
+
+        }
+
+        private void FrmCarGame_KeyUp(object sender, KeyEventArgs e)
+        {
+            _DirctionUAndD = enDirctionUAndD.eNull;
+            _DirctionLAndR = enDirctionLAndR.eNull;
+        }
+        int speedBotCar = -5;
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        void NewPointOfBotCar(PictureBox pictureBox)
+        {
+            if (pictureBox.Top + pictureBox.Height > panelGame.Height)
+            {
+                Random rand = new Random();
+                pictureBox.Location = new Point(rand.Next(0, panelGame.Width - pictureBox.Width), 0 - pictureBox.Height);
+            }
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            speedBotCar = Speed()-3;
+        
+
+            pictureBox1.Top += speedBotCar;
+            pictureBox2.Top += speedBotCar;
+            pictureBox3.Top += speedBotCar;
+            NewPointOfBotCar(pictureBox1);
+            NewPointOfBotCar(pictureBox2);
+            NewPointOfBotCar(pictureBox3);
+
+
+        }
+
+        private void labelScore_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        void restartTheGame()
+        {
+            pictureBox1.Top = -pictureBox1.Top;
+            pictureBox1.Left = 0;
+            pictureBox2.Top = -pictureBox2.Top;
+            pictureBox2.Left = 500;
+            pictureBox3.Top = -pictureBox3.Top;
+            pictureBox3.Left = 200;
+
+            minCar.Left = (panelGame.Width - pictureBox1.Width) / 2;
+            labelGameOver.Visible = false;
+            timerSpeed.Start();
+            timerBotCar.Start();
+            timerScore.Start();
+            _Score = 0;
+        }
+        private void labelGameOver_Click(object sender, EventArgs e)
+        {
+
+            restartTheGame();
+        }
+
+        int _Score = 0,_maxScore;
+
+       string  GetStringScore(bool maxSore=false)
+        {
+            string sScorse;
+            sScorse = $"Scorse : {_Score}";
+            if (maxSore)
+                sScorse = $"Mix Score : { _maxScore}";
+
+            return sScorse;
+        }
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            _Score+=Speed();
+            labelScore.Text=GetStringScore(false);
+            if(_maxScore<_Score)
+            {
+                _maxScore = _Score;
+                labelMaxScore.Text = GetStringScore(true);
+
+            }
+
+
+        }
+    }
+}
